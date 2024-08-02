d85c65c493ae72d8f2168cabbf03fd2a8698bff5
AnyueWang <moonwong.way@gmail.com>
2024-08-02 16:26:13 +0200

    add: different urls direct to different documents

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-var http = require('http')
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const port = 8080
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World!')
-}).listen(port, ()=>{
+  const url = req.url
+  let filePath
+  switch (url) {
+    case '/':
+      filePath = './index.html'
+      break;
+    case '/about':
+      filePath = './about.html'
+      break;
+    case '/contact-me':
+      filePath = './contact-me.html'
+      break;
+    default:
+      filePath = './404.html'
+      break;
+  }
+  fs.readFile(path.resolve(__dirname, filePath), (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(data)
+    return res.end()
+  })
+}).listen(port, () => {
   console.log(`The server is running on port ${port}`)
 })
+
